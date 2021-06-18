1f0db091b20daae4ffd55c619f5f508fbecb2e2e
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-18 13:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    let words= string.split(" ")
+    for(let i = 0; i < words.length; i++){
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    } return words.join(" ")
+  }
+  
+  static sanitize(string){
+    let words = string.split(" ")
+    for(let i = 0; i < words.length; i++){
+      words[i].toString()
+      words[i] = words[i].replace(/[^-,'A-Za-z0-9]+/g, '')
+    }return words.join(" ")
+  }
+   static titleize (string){
+    let words = string.split(" ")
+    return words.map(word => word[0].toUpperCase() + word.slice(1))
+    
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+Formatter.capitalize("bursting balloon")
